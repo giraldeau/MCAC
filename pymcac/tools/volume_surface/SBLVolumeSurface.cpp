19ebdd399bb4694ae08cd8482c008d452357c53a
Alexandre Poux <work@alexandrepoux.fr>
2020-02-18 15:24:09 +0100

    add copyright notice

--- pymcac/tools/volume_surface/SBLVolumeSurface.cpp
+++ pymcac/tools/volume_surface/SBLVolumeSurface.cpp
@@ -1,3 +1,20 @@
+/*
+ * MCAC
+ * Copyright (C) 2020 CORIA
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Regular_triangulation_euclidean_traits_3.h>
 #include <CGAL/Regular_triangulation_3.h>
