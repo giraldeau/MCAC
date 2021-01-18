4592021255377d889fc05a88726399058f3f1def
Alexandre Poux <work@alexandrepoux.fr>
2021-01-18 17:20:56 +0100

    update pymcac sbl wrapper

--- pymcac/tools/volume_surface/SBLVolumeSurface.cpp
+++ pymcac/tools/volume_surface/SBLVolumeSurface.cpp
@@ -15,37 +15,63 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+// CGAL VERSION less than 4.10 are note supported
+#define CGAL_VERSION_GEQ_4_10
+
+#include <SBLVolumeSurface.hpp>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+#ifndef CGAL_VERSION_GEQ_4_10
+#ifndef _CGAL_REGULAR_TRIANGULATION_EUCLIDEAN_TRAITS_3_H_
+#define _CGAL_REGULAR_TRIANGULATION_EUCLIDEAN_TRAITS_3_H_
 #include <CGAL/Regular_triangulation_euclidean_traits_3.h>
+#endif
+#else
+#ifndef _CGAL_REGULAR_TRIANGULATION_VERTEX_BASE_3_H_
+#define _CGAL_REGULAR_TRIANGULATION_VERTEX_BASE_3_H_
+#include <CGAL/Regular_triangulation_vertex_base_3.h>
+#endif
+#endif
+
 #include <CGAL/Regular_triangulation_3.h>
 #include <CGAL/Fixed_alpha_shape_vertex_base_3.h>
 #include <CGAL/Fixed_alpha_shape_cell_base_3.h>
 #include <CGAL/Fixed_alpha_shape_3.h>
-#include <CGAL/Exact_spherical_kernel_3.h>
 
-#include <SBLVolumeSurface.hpp>
+#include <CGAL/Exact_spherical_kernel_3.h>
 #include <SBL/GT/Union_of_balls_surface_volume_3.hpp>
 
 #include <iostream>
 #include <fstream>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel      K;
-typedef CGAL::Regular_triangulation_euclidean_traits_3<K>        Traits;
+
+// https://doc.cgal.org/latest/Alpha_shapes_3/Alpha_shapes_3_2ex_fixed_weighted_alpha_shapes_3_8cpp-example.html#_a6
+#ifdef CGAL_VERSION_GEQ_4_10
+typedef CGAL::Regular_triangulation_vertex_base_3<K>      Vbb;
+typedef CGAL::Fixed_alpha_shape_vertex_base_3<K,Vbb>      Vb;
+typedef CGAL::Regular_triangulation_cell_base_3<K>        Rcb;
+typedef CGAL::Fixed_alpha_shape_cell_base_3<K,Rcb>        Cb;
+typedef CGAL::Triangulation_data_structure_3<Vb,Cb>       Tds;
+typedef CGAL::Regular_triangulation_3<K,Tds>              Triangulation_3;
+typedef CGAL::Fixed_alpha_shape_3<Triangulation_3>        Alpha_complex;
+#else
+typedef K                                                        Traits;
 typedef CGAL::Fixed_alpha_shape_vertex_base_3<Traits>            Vb;
 typedef CGAL::Fixed_alpha_shape_cell_base_3<Traits>              Cb;
 typedef CGAL::Triangulation_data_structure_3<Vb, Cb>             Tds;
 typedef CGAL::Regular_triangulation_3<Traits, Tds>               Rt;
 typedef CGAL::Fixed_alpha_shape_3<Rt>                            Alpha_complex;
+#endif
 
-//#ifndef NDEBUG
-typedef CGAL::Exact_spherical_kernel_3                           SK;
-//#else // This does not work, crash execution if try to use it
-//typedef CGAL::Simple_cartesian<double>                           KE;
-//typedef CGAL::Algebraic_kernel_for_spheres_2_3<double>           AK;
-//typedef CGAL::Spherical_kernel_3<KE, AK>                         SK;
-//#endif
+//typedef CGAL::Interval_nt<false>                             FT;
+typedef double                                               FT;
+typedef CGAL::Simple_cartesian<FT>                                  KE;
+typedef CGAL::Algebraic_kernel_for_spheres_2_3<FT>           AK;
+typedef CGAL::Spherical_kernel_3<KE, AK>                     SK;
 
 typedef SBL::GT::T_Union_of_balls_surface_volume_3<Alpha_complex, SK>    Union_of_balls_surface_volume_3;
+
 
 
 void compute_volume_surface(const double spherelist[],
@@ -65,9 +91,6 @@ void compute_volume_surface(const double spherelist[],
 
   Union_of_balls_surface_volume_3 surface_volume(spheres.begin(), spheres.end());
 
-  CGAL::Interval_nt<false> cgalvolume = surface_volume.volume();
-  CGAL::Interval_nt<false> cgalarea = surface_volume.area();
-
-  volume = to_double(cgalvolume);
-  area = to_double(cgalarea);
+  volume = to_double(surface_volume.volume());
+  area = to_double(surface_volume.area());
 }
