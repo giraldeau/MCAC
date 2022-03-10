9e2920aedc839a79112efb79f69527057f9c8710
Alexandre Poux <work@alexandrepoux.fr>
2022-03-10 11:08:14 +0100

    sbl is not required anymore for pymcac

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # coding=utf-8
 """Compile and install pymcac"""
-from Cython.Build import cythonize
+from pathlib import Path
 
+from Cython.Build import cythonize
 from numpy.distutils.core import Extension, setup
 
+ext_modules = []
 coverage = Extension(
     name="pymcac.tools.coverage.coverages_cython",
     sources=[
@@ -15,20 +17,23 @@ coverage = Extension(
     extra_link_args=["-fopenmp"],
 )
 (coverage,) = cythonize(coverage, build_dir="build")
+ext_modules.append(coverage)
 
-sbl = Extension(
-    name="pymcac.tools.volume_surface.sbl_wrapper",
-    sources=[
-        "pymcac/tools/volume_surface/sbl_wrapper.pyx",
-        "pymcac/tools/volume_surface/SBLVolumeSurface.cpp",
-    ],
-    include_dirs=["pymcac/tools/volume_surface", "ext_bin/sbl/include", "/opt/cgal/include"],
-    library_dirs=["/opt/cgal/lib64"],
-    extra_compile_args=["-fopenmp", "-O3", "-frounding-math", "-DNDEBUG"],
-    libraries=["mpfr", "gmp"],
-    extra_link_args=["-fopenmp"],
-    language="c++",
-)
-(sbl,) = cythonize(sbl, build_dir="build")
+if Path("ext_bin/sbl/include").exists():
+    sbl = Extension(
+        name="pymcac.tools.volume_surface.sbl_wrapper",
+        sources=[
+            "pymcac/tools/volume_surface/sbl_wrapper.pyx",
+            "pymcac/tools/volume_surface/SBLVolumeSurface.cpp",
+        ],
+        include_dirs=["pymcac/tools/volume_surface", "ext_bin/sbl/include", "/opt/cgal/include"],
+        library_dirs=["/opt/cgal/lib64"],
+        extra_compile_args=["-fopenmp", "-O3", "-frounding-math", "-DNDEBUG"],
+        libraries=["mpfr", "gmp"],
+        extra_link_args=["-fopenmp"],
+        language="c++",
+    )
+    (sbl,) = cythonize(sbl, build_dir="build")
+    ext_modules.append(sbl)
 
-setup(ext_modules=[coverage, sbl])
+setup(ext_modules=ext_modules)
